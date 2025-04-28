bbbacd79669d466f6547aec1235a2a64b0e4e383
KaranParelkar <143124504+KaranParelkar@users.noreply.github.com>
2025-04-29 01:19:27 +0530

    Show/Hide(Text)_project

---
+++ Show-Hide-Text-Project/App.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import './App.css';
+import {Text} from './Text'
+
+function App() {
+const [showText,setShowText] = useState(false);
+return(
+  <div className='App'>
+    <button
+    onClick={() => {
+      setShowText(!showText);
+    }}
+    >Show/Hide Text
+    </button>
+
+    {showText && <Text />}
+  </div>
+)
+}
+
+export default App;
